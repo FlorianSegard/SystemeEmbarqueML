a4180471b5d900e8d97815c6c07783163f57dc7e
Florian <floriansegard@gmail.com>
2024-10-24 15:23:36 +0200

    ok

--- docker_pull_and_run.sh
+++ docker_pull_and_run.sh
@@ -1,2 +1,20 @@
-docker pull floriansgd/embarqueml:latest;
-docker run -p 8114:8114 -d floriansgd/embarqueml:latest
+#!/bin/bash
+
+CONTAINER_NAME="house-price-prediction"
+DOCKER_IMAGE="floriansgd/embarqueml:latest"
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Container $CONTAINER_NAME is running. Stopping it..."
+    
+    docker stop $CONTAINER_NAME
+    
+    echo "Container $CONTAINER_NAME stopped."
+else
+    echo "Container $CONTAINER_NAME is not running."
+fi
+
+docker rm $CONTAINER_NAME
+
+echo "Starting a new instance of $CONTAINER_NAME..."
+docker run -d --name $CONTAINER_NAME -p 8114:8114 $DOCKER_IMAGE
+echo "Container $CONTAINER_NAME started."
